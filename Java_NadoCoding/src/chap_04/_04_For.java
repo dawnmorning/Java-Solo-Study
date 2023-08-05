722644743acdb0027df6c58ac657605955c774e6
김종혁 <notify9637@naver.com>
2023-08-06 08:41:54 +0900

    배열 기본

---
+++ Java_NadoCoding/src/chap_04/_04_For.java
@@ -0,0 +1,12 @@
+package chap_04;
+
+public class _04_For {
+    public static void main(String[] args) {
+        for (int i = 0; i<10; i++){
+            System.out.println(i + " 번째 증가");
+        }
+        for (int i = 5; i>0; i--){
+            System.out.println(i + " 감소");
+        }
+    }
+}
---
+++ Java_NadoCoding/src/chap_05/_01_Array.java
@@ -0,0 +1,15 @@
+package chap_05;
+
+public class _01_Array {
+    public static void main(String[] args) {
+        // 배열 : 같은 자료형의 연속된 값을 저장하는 공간
+//        String[] Coffees = new String[4];
+//        String Coffees[] = new String[4];
+//        String[] Coffees = new String[]{"아메리카노", "카페모카", "라떼", "카푸치노"};
+        String[] Coffees = {"아메리카노", "카페모카", "라떼", "카푸치노"};
+
+        int[] i = new int[3];
+        double[] d = new double[]{10.0, 11.2, 13.5};
+        boolean[] b = {true, true, false};
+    }
+}
