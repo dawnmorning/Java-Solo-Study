cc90b6eccd663b0cf114a945b9849347c81cc965
김종혁 <notify9637@naver.com>
2023-08-06 09:00:33 +0900

    Method와 Method 오버로딩

---
+++ Java_NadoCoding/src/chap_06/_01_Method.java
@@ -0,0 +1,10 @@
+package chap_06;
+
+public class _01_Method {
+    public static void SayHello(){
+        System.out.println("안녕하세요");
+    }
+    public static void main(String[] args) {
+        SayHello();
+    }
+}
---
+++ Java_NadoCoding/src/chap_06/_02_Parameter.java
@@ -0,0 +1,10 @@
+package chap_06;
+
+public class _02_Parameter {
+    public static void power(int number){
+        System.out.println(number * number);
+    }
+    public static void main(String[] args) {
+    power(2);
+    }
+}
